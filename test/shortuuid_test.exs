7c68cfdf7f925b2e28375ac2e8b14ba7fa231818
Goran Pedic <gpedic@gmail.com>
2017-08-18 13:10:59 +0200

    add test for curly braces uuid

--- test/shortuuid_test.exs
+++ test/shortuuid_test.exs
@@ -8,4 +8,9 @@ defmodule ShortUUIDTest do
     assert encoded_niluuid == ""
     assert ShortUUID.decode(encoded_niluuid ) == niluuid
   end
+
+  test "handles curly braces in uuid" do
+    uuid = "{2a162ee5-02f4-4701-9e87-72762cbce5e2}"
+    assert ShortUUID.encode(uuid) == "keATfB8JP2ggT7U9JZrpV9"
+  end
 end
